ad844358877ccdbb8bbf339609052ad47dcaa9c7
wjdwithyou <asdf142857@gmail.com>
2017-02-20 01:00:04 +0900

    initial commit

---
+++ app/src/main/java/xyz/tastewiki/tastewiki_for_android/MainActivity.java
@@ -0,0 +1,96 @@
+package xyz.tastewiki.tastewiki_for_android;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webViewMain;
+    Button button1, button2, recommendButton;
+    View.OnClickListener cListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webViewMain = (WebView)findViewById(R.id.webViewMain);
+        button1 = (Button)findViewById(R.id.button1);
+        button2 = (Button)findViewById(R.id.button2);
+        recommendButton = (Button)findViewById(R.id.recommendButton);
+        cListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.button1:
+                        clickButton1();
+                        break;
+                    case R.id.button2:
+                        clickButton2();
+                        break;
+                    case R.id.recommendButton:
+                        recommendSpot();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        };
+
+        button1.setOnClickListener(cListener);
+        button2.setOnClickListener(cListener);
+        recommendButton.setOnClickListener(cListener);
+
+
+
+        WebSettings webSettings = webViewMain.getSettings();
+
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setBuiltInZoomControls(true);
+
+        webViewMain.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+            }
+        });
+        webViewMain.setWebChromeClient(new WebChromeClient() {
+
+        });
+
+        webViewMain.loadUrl("https://www.tastewiki.xyz");
+    }
+
+    @Override
+    public void onBackPressed() {
+        // super.onBackPressed();
+        if (webViewMain.canGoBack()) {
+            webViewMain.goBack();
+        } else {
+            finish();
+        }
+    }
+
+    void clickButton1() {
+
+    }
+
+    void clickButton2() {
+
+    }
+
+    void recommendSpot() {
+        webViewMain.loadUrl("javascript:recommendSpot();");
+    }
+}
