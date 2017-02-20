14c323bc49b4c033f9020cb45d453bbe5832705e
wjdwithyou <asdf142857@gmail.com>
2017-02-20 17:53:52 +0900

    permission for location (not complete)

--- app/src/main/java/xyz/tastewiki/tastewiki_for_android/MainActivity.java
+++ app/src/main/java/xyz/tastewiki/tastewiki_for_android/MainActivity.java
@@ -1,6 +1,14 @@
 package xyz.tastewiki.tastewiki_for_android;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +19,10 @@ import android.webkit.WebViewClient;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_ACCESS_COARSE_LOCATION = 1;
+    private static final int REQEUST_ACCESS_FINE_LOCATION = 2;
+
+    final LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
     WebView webViewMain;
     Button button1, button2, recommendButton;
     View.OnClickListener cListener;
@@ -20,10 +32,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_ACCESS_COARSE_LOCATION);
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQEUST_ACCESS_FINE_LOCATION);
+        }
+
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, 1, mLocationListener);
+        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 100, 1, mLocationListener);
+
         webViewMain = (WebView)findViewById(R.id.webViewMain);
+
         button1 = (Button)findViewById(R.id.button1);
         button2 = (Button)findViewById(R.id.button2);
         recommendButton = (Button)findViewById(R.id.recommendButton);
+
         cListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -71,6 +96,66 @@ public class MainActivity extends AppCompatActivity {
 
         webViewMain.loadUrl("https://www.tastewiki.xyz");
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case REQUEST_ACCESS_COARSE_LOCATION:
+                for (int i = 0; i < permissions.length; ++i) {
+                    if (permissions[i].equals(Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                        if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+
+                        }
+                        else {
+
+                        }
+                    }
+                }
+                break;
+            case REQEUST_ACCESS_FINE_LOCATION:
+                for (int i = 0; i < permissions.length; ++i) {
+                    if (permissions[i].equals(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                        if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+
+                        }
+                        else {
+
+                        }
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private final LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            double lat = location.getLatitude();
+            double lng = location.getLongitude();
+
+            //Log.d("Lat", lat);
+            //Log.d("Lng", lng);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
 
     @Override
     public void onBackPressed() {
